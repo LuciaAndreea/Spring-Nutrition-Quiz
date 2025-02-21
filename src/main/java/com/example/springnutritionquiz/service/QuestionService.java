1e99a22106e87f6124e90557b9cc6badd7acf16e
Lucia <luciaandreeaursu@yahoo.com>
2025-02-21 12:43:29 +0200

    Create a Service annotation for the logic behind the app

---
+++ src/main/java/com/example/springnutritionquiz/service/QuestionService.java
@@ -0,0 +1,20 @@
+package com.example.springnutritionquiz.service;
+
+import com.example.springnutritionquiz.Question;
+import com.example.springnutritionquiz.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+
+    public List<Question> getAllQuestions() {
+       return questionDao.findAll();
+    }
+}
