ac5f2a5e427917be77907c03c12dc8cd0bb4849f
Lucia <luciaandreeaursu@yahoo.com>
2025-02-21 12:40:29 +0200

    Create a REST Controller to handle requests for all questions

--- src/main/java/com/example/springnutritionquiz/controller/QuestionController.java
+++ src/main/java/com/example/springnutritionquiz/controller/QuestionController.java
@@ -1,14 +1,23 @@
 package com.example.springnutritionquiz.controller;
 
+import com.example.springnutritionquiz.Question;
+import com.example.springnutritionquiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions() {
-        return "All Questions";
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
     }
 }
