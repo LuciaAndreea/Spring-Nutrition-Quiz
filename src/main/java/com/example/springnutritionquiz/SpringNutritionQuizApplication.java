6d7ccb3b32bb5bbb6f85845af955245177e42698
Lucia <luciaandreeaursu@yahoo.com>
2025-02-21 12:21:37 +0200

    Initial commit

---
+++ src/main/java/com/example/springnutritionquiz/SpringNutritionQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnutritionquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNutritionQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNutritionQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springnutritionquiz/SpringNutritionQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springnutritionquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNutritionQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
