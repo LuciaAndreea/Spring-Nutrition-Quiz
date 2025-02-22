9d380b8de6872b17ee5b932bf9f5f747a10ddde5
Lucia <luciaandreeaursu@yahoo.com>
2025-02-22 21:11:37 +0200

    Create a method in the service for calculating quiz responses

--- src/main/java/com/example/springnutritionquiz/service/QuizService.java
+++ src/main/java/com/example/springnutritionquiz/service/QuizService.java
@@ -5,6 +5,7 @@ import com.example.springnutritionquiz.dao.QuizDao;
 import com.example.springnutritionquiz.model.Question;
 import com.example.springnutritionquiz.model.QuestionWrapper;
 import com.example.springnutritionquiz.model.Quiz;
+import com.example.springnutritionquiz.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,5 +48,22 @@ public class QuizService {
        }
 
        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(int id, List<Response> responses) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+
+        int right = 0;
+        int i =0;
+
+        for(Response response: responses){
+            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
+                right++;
+
+            i++;
+        }
+
+        return new ResponseEntity<>(right, HttpStatus.OK);
     }
 }
